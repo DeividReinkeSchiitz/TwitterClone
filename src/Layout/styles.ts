a4b691ee1d23326e48c2db136400aa2b7790df31
Deivid R. S. <deivid@schiitz.com>
2024-10-22 15:51:17 -0400

    header

--- src/Layout/styles.ts
+++ src/Layout/styles.ts
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
   background: var(--primary);
   height: auto;
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `;
 
 export const Wrapper = styled.div`
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,6 +1,20 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+  :root{
+    --primary: #000;
+    --secondary: #15181C;
+    --search: #202327;
+    --white: #D9D9D9;
+    --gray: #7A7A7A;
+    --outline: #2F3336;
+    --retweet: #00C06B;
+    --like: #E8265E;
+    --twitter: #33A1F2;
+    --twitter-dark-hover: #011017;
+    --twitter-light-hover: #2C8ED6;
+  }
+
   * {
     margin:0;
     padding:0;
@@ -23,19 +37,5 @@ export default createGlobalStyle`
 
   html{
     background-color:var(--primary);
-  }
-
-  :root{
-    --primary: #000;
-    --secondary: #15181C;
-    --search: #202327;
-    --white: #D9D9D9;
-    --gray: #7A7A7A;
-    --outline: #2F3336;
-    --retweet: #00C06B;
-    --like: #E8265E;
-    --twitter: #33A1F2;
-    --twitter-dark-hover: #011017;
-    --twitter-light-hover: #2C8ED6;
   }
 `;
